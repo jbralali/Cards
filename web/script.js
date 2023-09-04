c54ca99eb56ec5c17ffaba3f67184f76c7dd7b1f
jbralali <jbral@DESKTOP-VNVD5JP>
2023-09-05 01:41:56 +0300

    11

--- web/script.js
+++ web/script.js
@@ -1,16 +1,31 @@
-function saveToDatabase(data) {
-  fetch('/save', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
+function enableEditing(elementId) {
+  var element = document.getElementById(elementId);
+  var currentText = element.textContent;
+
+  if (element.getElementsByTagName('input').length > 0) {
+    return;
+  }
+
+  var isEmail = (elementId === 'email') ? 'type="email"' : 'type="text"';
+  var inputHTML = `<input ${isEmail} id="${elementId}Input" value="${currentText.replace('Email: ', '')}" onblur="saveText('${elementId}')">`;
+
+  element.innerHTML = (elementId === 'email') ? 'Email: ' + inputHTML : inputHTML;
+}
+
+function saveText(elementId) {
+  var inputElement = document.getElementById(elementId + "Input");
+  var newText = inputElement.value;
+
+  var element = document.getElementById(elementId);
+  element.innerHTML = (elementId === 'email') ? 'Email: ' + newText : newText;
+}
+
+function saveAll() {
+  var fields = ['generatedName', 'yourTitle', 'email'];
+  fields.forEach(function(field) {
+    saveText(field);
   });
+
+  // Simulate save to database here
+  alert('Data saved (simulated)');
 }
